2936da4a0144be1bec3b8c6d5342ab70200e5cb2
Lars Mathiesen <larswmat@gmail.com>
2024-06-15 13:15:58 +0200

    added text color + bg color modified backspace func to use utf to avoid crash changed font to press2play

--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@ if arg[2] == "debug" then
 end
 
 
+local utf8 = require("utf8")
 local game_manager = require "src.game_manager"
+
 
 local text_buffer_list = {
     textInput = ""
@@ -29,7 +31,26 @@ local settings = {
     fullscreen = false,
     screenScaler = 1,
     width = 640,
-    height = 360
+    height = 360,
+    -- grey: #333a45 ,51,58,69 
+    -- grey
+    background_color =  {
+        r = 51,
+        g = 58,
+        b = 69
+    },
+    -- white
+    text_color_base = {
+        r = 244,
+        g = 244,
+        b = 244
+    }, 
+    -- pink
+    text_color_user_intput = { 
+        r = 244,
+        g = 76,
+        b = 127
+    }
 }
 -- global mouse variables to hold correct mouse pos in the scaled world 
 mouse_x, mouse_y = ...
@@ -44,15 +65,18 @@ function love.load()
     -- Set up the window with resizable option
     love.window.setMode(settings.width, settings.height, {resizable=true, vsync=0, minwidth=settings.width*settings.screenScaler, minheight=settings.height*settings.screenScaler})
     -- font = love.graphics.newFont('fonts/m6x11.ttf', 16)
-    -- font = love.graphics.newFont('fonts/PressStart2P-Regular.ttf', 16)
+    -- siez: 8, 16, 24 ...
+    font = love.graphics.newFont('fonts/PressStart2P-Regular.ttf', 16)
+    -- font = love.graphics.newFont('fonts/RobotoMono-VariableFont_wght.ttf', 20)
     -- https://ggbot.itch.io/pixeloid-font
     -- pixeloid sizes: 9, 18, 36, 72, 144
     -- size 18 = 10x14 + 2 px space  ~.... 12x14 
-    font = love.graphics.newFont('fonts/PixeloidMono.ttf', 18)
-    
+    -- font = love.graphics.newFont('fonts/PixeloidMono.ttf', 18)
+    love.graphics.setBackgroundColor( settings.background_color.r/255, settings.background_color.g/255, settings.background_color.b/255)
+
     
     love.graphics.setFont(font)
-    -- love.graphics.setDefaultFilter("nearest", "nearest")
+    love.graphics.setDefaultFilter("nearest", "nearest")
     read_text_file_to_table()
 end
 
@@ -74,19 +98,27 @@ function love.draw()
     love.graphics.print("mouse: " .. mouse_x .. "," .. mouse_y, 1, 1)
 
     -- love.graphics.print(text,x,y,r,sx,sy,ox,oy)
-    -- text can be written between x=24 and x=622
-    -- max 50 chars on a single line
-    -- 18 pixel beteween each line
-    -- max 18 lines
+    -- text can be written between x=28 and x=612
+    -- max 37 chars on a single line
+    -- 20 pixel beteween each line
+    -- max 16 lines
     -- first line start at x = 18
-    for i = 1, 18, 1 do
-        y = i * 18
-        
+    -- ---------------------------------
+    -- test lines on screen
+    for i = 1, 16, 1 do
+        y = i * 20
+    ---------------------------------
         love.graphics.print(i .. " some where over the rainbow", 1, y )    
         
     end
+    -- set user input text color
+
+    love.graphics.print("Somewhere over the rainbow ^^", 24, settings.height/2)
+    love.graphics.setColor(settings.text_color_user_intput.r/255,settings.text_color_user_intput.g/255, settings.text_color_user_intput.b/255)
     love.graphics.print(text_buffer_list.textInput, 24, settings.height/2)
-    love.graphics.print("AA", 622, settings.height/2)
+    love.graphics.print("AA", 612, settings.height/2)
+    -- reset text color to base
+    love.graphics.setColor(settings.text_color_base.r/255,settings.text_color_base.g/255, settings.text_color_base.b/255)
     love.graphics.print(#text_buffer_list.textInput, 1, 20)
 
     love.graphics.circle('fill', mouse_x, mouse_y, 5)
@@ -128,9 +160,14 @@ function love.keypressed(key)
         end 
     end
 
+ 
     if key == 'backspace' then
-        -- text:sub(1, #text - 1)
-        text_buffer_list.textInput = string.sub(text_buffer_list.textInput, 1, -2)
+        -- Remove the last UTF-8 character, 
+        -- we need to use utf8.offset in order for the system not to crash 
+        local byteoffset = utf8.offset(text_buffer_list.textInput, -1)
+        if byteoffset then
+            text_buffer_list.textInput = string.sub(text_buffer_list.textInput, 1, byteoffset - 1)
+        end
     end
 end
 
